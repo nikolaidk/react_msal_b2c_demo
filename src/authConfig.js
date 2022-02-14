4dac3faae8d9796d95eea97c3eddc4f0347b926a
nikolaidk <nmr@continia.com>
2022-02-14 12:08:16 +0100

    Modified AD config

--- src/authConfig.js
+++ src/authConfig.js
@@ -1,8 +1,8 @@
 // Config object to be passed to Msal on creation
 export const msalConfig = {
     auth: {
-        clientId: "6dccb97a-8c76-44c1-9319-e89a60866206",
-        authority: "https://continiadevb2c.b2clogin.com/continiadevb2c.onmicrosoft.com/B2C_1_ContiniaLearnDev",
+        clientId: "11ec2c01-4106-43e7-8651-ba783a948865",
+        authority: "https://continiadevb2c.b2clogin.com/continiadevb2c.onmicrosoft.com/B2C_1_demo",
         knownAuthorities: ["continiadevb2c.b2clogin.com"],
         redirectUri: "http://localhost:3000",
         postLogoutRedirectUri: "http://localhost:3000"
@@ -10,5 +10,5 @@ export const msalConfig = {
 };
 
 export const loginRequest = {
-    scopes: ["https://continiadevb2c.onmicrosoft.com/api/demo.read"]
+    scopes: ["https://continiadevb2c.onmicrosoft.com/50d38c5b-3504-485e-b172-4e7dd494f44e/demo.read"]
 };
